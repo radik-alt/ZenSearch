76c15607aed888d9e9e2367141a90d3af78e4ed6
Владимир Афанасьев <vladimir.afanasev@irlix.ru>
2022-10-21 13:03:45 +0400

    add firebase

---
+++ app/src/main/java/App.kt
@@ -0,0 +1,25 @@
+package com.example.zensearch
+
+import android.app.Application
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+            token = task.result
+        })
+    }
+
+    companion object {
+        private lateinit var token: String
+
+        fun getToken() = token
+    }
+}
