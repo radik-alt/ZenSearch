724a0d06327c4c15a084c65793e554f022da8eee
Radik <Radik.app@yandex.ru>
2022-10-21 11:53:22 +0400

    Initial commit

---
+++ app/src/main/java/com/example/zensearch/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zensearch.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zensearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
